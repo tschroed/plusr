a6e869d3199619de23a90794bd2972424b09dbfc
tschroed <tschroed@gmail.com>
2013-12-18 08:34:02 -0500

    gofmt the code.

--- picasa.go
+++ picasa.go
@@ -1,28 +1,28 @@
 package picasa
 
 import (
-  "encoding/xml"
+	"encoding/xml"
 )
 
 type Content struct {
-  Src string `xml:"src,attr"`
+	Src string `xml:"src,attr"`
 }
 
 type Photo struct {
-  Contents Content `xml:"content"`
-  Id string `xml:"id"`
-  Version string `xml:"version"`
-  Title string `xml:"title"`
-  Album string `xml:"albumtitle"`
+	Contents Content `xml:"content"`
+	Id       string  `xml:"id"`
+	Version  string  `xml:"version"`
+	Title    string  `xml:"title"`
+	Album    string  `xml:"albumtitle"`
 }
 
 type PhotoFeed struct {
-  XMLName xml.Name `xml:"feed"`
-  Photos []Photo `xml:"entry"`
+	XMLName xml.Name `xml:"feed"`
+	Photos  []Photo  `xml:"entry"`
 }
 
 func ParseFeed(text []byte) PhotoFeed {
-  feed := PhotoFeed{}
-  xml.Unmarshal([]byte(text), &feed)
-  return feed
+	feed := PhotoFeed{}
+	xml.Unmarshal([]byte(text), &feed)
+	return feed
 }
