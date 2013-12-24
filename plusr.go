6fbbef66c718225a49495da557426adb28a0d8ae
tschroed <tschroed@gmail.com>
2013-12-24 16:24:35 -0500

    gofmt

--- plusr.go
+++ plusr.go
@@ -1,79 +1,79 @@
 package plusr
 
 import (
-    "html/template"
-    "net/http"
-    "time"
+	"html/template"
+	"net/http"
+	"time"
 
-    "appengine"
-    "appengine/datastore"
-    "appengine/user"
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 )
 
 type Greeting struct {
-    Author  string
-    Content string
-    Date    time.Time
+	Author  string
+	Content string
+	Date    time.Time
 }
 
 func init() {
-    http.HandleFunc("/", root)
-    http.HandleFunc("/logout", logout)
-    http.HandleFunc("/sign", sign)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/sign", sign)
 }
 
 // guestbookKey returns the key used for all guestbook entries.
 func guestbookKey(c appengine.Context) *datastore.Key {
-    // The string "default_guestbook" here could be varied to have multiple guestbooks.
-    return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-  var url string
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    if u != nil {
-      var err error
-      url, err = user.LogoutURL(c, r.URL.String())
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-          return
-      } 
-    } else {
-	url = "/"
-    }
-    w.Header().Set("Location", url)
-    w.WriteHeader(http.StatusFound)
-    return
-  }
+	var url string
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u != nil {
+		var err error
+		url, err = user.LogoutURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		url = "/"
+	}
+	w.Header().Set("Location", url)
+	w.WriteHeader(http.StatusFound)
+	return
+}
 
 func root(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    if u == nil {
-      url, err := user.LoginURL(c, r.URL.String())
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-          return
-      }
-      w.Header().Set("Location", url)
-      w.WriteHeader(http.StatusFound)
-      return
-    }
-    // Ancestor queries, as shown here, are strongly consistent with the High
-    // Replication Datastore. Queries that span entity groups are eventually
-    // consistent. If we omitted the .Ancestor from this query there would be
-    // a slight chance that Greeting that had just been written would not
-    // show up in a query.
-    q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
-    greetings := make([]Greeting, 0, 10)
-    if _, err := q.GetAll(c, &greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    if err := guestbookTemplate.Execute(w, greetings); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	// Ancestor queries, as shown here, are strongly consistent with the High
+	// Replication Datastore. Queries that span entity groups are eventually
+	// consistent. If we omitted the .Ancestor from this query there would be
+	// a slight chance that Greeting that had just been written would not
+	// show up in a query.
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(c)).Order("-Date").Limit(10)
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(c, &greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(w, greetings); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
@@ -101,22 +101,22 @@ const guestbookTemplateHTML = `
 `
 
 func sign(w http.ResponseWriter, r *http.Request) {
-    c := appengine.NewContext(r)
-    u := user.Current(c)
-    g := Greeting{
-        Content: r.FormValue("content"),
-        Date:    time.Now(),
-    }
-    g.Author = u.String()
-    // We set the same parent key on every Greeting entity to ensure each Greeting
-    // is in the same entity group. Queries across the single entity group
-    // will be consistent. However, the write rate to a single entity group
-    // should be limited to ~1/second.
-    key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
-    _, err := datastore.Put(c, key, &g)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/", http.StatusFound)
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	g := Greeting{
+		Content: r.FormValue("content"),
+		Date:    time.Now(),
+	}
+	g.Author = u.String()
+	// We set the same parent key on every Greeting entity to ensure each Greeting
+	// is in the same entity group. Queries across the single entity group
+	// will be consistent. However, the write rate to a single entity group
+	// should be limited to ~1/second.
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
