2cc73c737b29613c0598af63c72a435d1b363a34
tschroed <tschroed@gmail.com>
2014-01-09 21:22:39 -0500

    Working (junky) upload based on changes to oauth and flickgo.

--- flickr/flickr.go
+++ flickr/flickr.go
@@ -2,20 +2,40 @@ package flickr
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
+	"time"
 
 	"appengine"
+	//	"appengine/urlfetch"
 	"appengine/user"
 
 	"code.google.com/p/flickgo"
 )
 
+func (uc *userConfig) Do(req *http.Request) (resp *http.Response, err error) {
+	consumer := uc.oauthConsumer()
+	tok, _ := uc.accessToken()
+	return consumer.Do(req, tok)
+}
+
+func (uc *userConfig) Head(url string) (resp *http.Response, err error) {
+	return nil, errors.New("Not Implemented.")
+}
+
 func (uc *userConfig) Get(url string) (resp *http.Response, err error) {
 	consumer := uc.oauthConsumer()
 	tok, _ := uc.accessToken()
 	return consumer.Get(url, nil, tok)
+}
+
+func (uc *userConfig) PostForm(url string, data url.Values) (resp *http.Response, err error) {
+	return nil, errors.New("Not Implemented.")
 }
 
 func (uc *userConfig) Post(url string, bodyType string, body io.Reader) (resp *http.Response, err error) {
@@ -29,14 +49,62 @@ func (uc *userConfig) Post(url string, bodyType string, body io.Reader) (resp *h
 	return consumer.Post(url, buf.String(), nil, tok)
 }
 
+func readImage() ([]byte, error) {
+	f, err := os.Open("/tmp/test-pattern.jpg")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ioutil.ReadAll(f)
+}
+
 func UploadGarbage(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	uc := userConfig{context: c, rootUser: user.Current(c).String()}
-	uc.oauthConsumer().Debug(true)
+	consumer := uc.oauthConsumer()
+	consumer.Debug(true)
 	fc := flickgo.New(APIKey, APISecret, &uc)
-	username, _, err := fc.TestLogin()
-	c.Infof("username, err: %v, %v", username, err)
-	buf := bytes.NewBufferString("This is a bunch of crap.\r\n")
-	tick, err := fc.Upload("Garbage", buf.Bytes(), nil)
-	uc.context.Infof("tick, err: %v, %v", tick, err)
+	fc.Logger = uc.context
+	c.Infof("==== TESTING LOGIN USING LEGACY CODE ====")
+	username, id, err := fc.TestLogin()
+	c.Infof("username, id, err: %v, %v, %v", id, username, err)
+	/*
+	        c.Infof("==== TESTING LOGIN USING NEW CODE ====")
+	        host := "www.flickr.com"
+	        path := "/services/rest?method=flickr.test.login"
+	        req, err := http.NewRequest("GET",
+	                                    fmt.Sprintf("http://%s%s", host, path), nil)
+	        if err != nil {
+	                uc.context.Errorf("NewRequest: %v", err)
+	                return
+	        }
+		tok, _ := uc.accessToken()
+	        resp, err := consumer.Do(req, tok)
+	        if err != nil {
+	                uc.context.Errorf("Do error: %v", err)
+	                return
+	        }
+		b, err := ioutil.ReadAll(resp.Body)
+	        defer resp.Body.Close()
+		if err != nil {
+	                uc.context.Errorf("ReadAll error: %v", err)
+			return
+		}
+	        uc.context.Infof("%v", bytes.NewBuffer(b).String())
+	*/
+	c.Infof("==== TESTING UPLOAD USING NEW CODE ====")
+	img, err := readImage()
+	if err == nil {
+		buf := bytes.NewBuffer(img)
+		tick, err := fc.Upload(fmt.Sprintf("Test Pattern at %v", time.Now()),
+			buf.Bytes(), nil)
+		uc.context.Infof("tick, err: %v, %v", tick, err)
+	}
+	/*
+	        c.Infof("==== PLAIN OLD POST VIA CONTEXT ====")
+		buf = bytes.NewBuffer("This is a bunch of crap. Foo.\r\n")
+	        client := urlfetch.Client(uc.context)
+	        resp, err = client.Post("http://www.google.com/", "text/plain", buf)
+	        uc.context.Infof("Post: %#v, %#v", resp, err)
+	*/
 }
